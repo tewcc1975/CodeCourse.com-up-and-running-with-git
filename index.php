e0d29e3286b7a84bddc59b862622789ed5e25be9
tewcc1975 <tewcc1975@gmail.com>
2020-02-11 20:14:21 +0800

    Fix homepage

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require 'vendor/autoload.php';
 $app = new Slim\App();
 
 $app->get('/', function($request, $response, $args) {
-	return $response->write('Home');
+	return $response->write('Home!');
 });
 
 $app->get('/contact', function($request, $response, $args) {
