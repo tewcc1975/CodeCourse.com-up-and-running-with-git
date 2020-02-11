6038d998a85c36aca1b9d05155577842b1f5c84f
tewcc1975 <tewcc1975@gmail.com>
2020-02-11 15:04:21 +0800

    Contact design

--- index.php
+++ index.php
@@ -7,4 +7,8 @@ $app->get('/', function($request, $response, $args) {
 	return $response->write('Home');
 });
 
+$app->get('/contact', function($request, $response, $args) {
+	return $response->write('Contact');
+});
+
 $app->run();
